61be1234a235b1fad6c849f520df75279db3ae0b
GokulNath20 <61192087+GokulNath20@users.noreply.github.com>
2020-03-12 15:27:37 +0800

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,42 @@
+public class Test1 {//class name
+   
+   static byte gg;
+   static short ff;
+   static int max;
+   static long lol;
+   static float ww;     //all primitive data types of Java have been declared
+   static double yy;
+   static char tt;
+   static boolean truth_or_dare;
+   
+   public static void main (String[] args){
+   
+   System.out.println("Hi guyzz...My name is Gokul..Bubye ahhh...\n"); //display message(Quetion 1)
+   
+   
+   
+   System.out.println("Default value of \"byte\" is " + gg);
+   System.out.println("Default value of \"short\" is " + ff);
+   System.out.println("Default value of \"int\" is " + max);
+   System.out.println("Default value of \"long\" is " + lol);
+   System.out.println("Default value of \"float\" is " + ww);
+   System.out.println("Default value of \"double\" is " + yy);
+   System.out.println("Default value of \"char\" is " + tt);
+   System.out.println("Default value of \"boolean\" is " + truth_or_dare+"\n");
+   //display default value of all primitive data types of Java (Question 2)
+   
+   String dude = "Dr.Azmin";
+   String perfect = "Dr.Azman";//two String objects have been declared and initialized
+   
+   if(dude.equals(perfect))//compare two strings(Question 3)
+      
+      System.out.println("Ohh yeah :)");
+      
+   else
+   
+      System.out.println("Ohh noo :(");
+
+   
+   }
+   
+ }
